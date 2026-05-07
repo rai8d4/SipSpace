8339928bad0cf2bbcab4e486f5832d057840a9be
rai8d4 <122903297+rai8d4@users.noreply.github.com>
2026-05-07 10:29:15 -0500

    added database Mongo connection w Mongoose in db.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const router = require("./router");
+const connectDB = require("./db");
 
 const app = express();
+
+connectDB();
 
 app.use(cors());
 app.use(express.json());
---
+++ router.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+//temp stuff for rn PLS IGNORE --> node isn't starting server with enpty router file LoLs
+
+
+module.exports = router;
